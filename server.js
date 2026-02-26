00e908ba339d3c80aef817495988174aaef865c4
mavagoncalves <mavagoncalves@gmail.com>
2026-02-26 16:29:11 +0100

    Fix: forcing node to use IPv4 isntead of IPv6

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI{
+  family: 4 //forces Node to use standard IPv4
+})
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
