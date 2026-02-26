c8da67e04c5ec65adc6270470507277d4e925e76
mavagoncalves <mavagoncalves@gmail.com>
2026-02-26 16:09:43 +0100

    Connecting to the server

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+//control for data --> schema
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
+  })
+  .catch(err => console.log(err));
