00a8f3e15f4c41679ebbc04232f2f14d0b21fe61
mavagoncalves <mavagoncalves@gmail.com>
2026-03-09 18:43:26 +0100

    Functionality to login the user into the db

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,4 +20,21 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// LOGIN user to db
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Find user in DB
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    // Compare hashed password
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) return res.status(400).json({ message: "Invalid password" });
+
+    // Create Token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+});
+
 module.exports = router;
