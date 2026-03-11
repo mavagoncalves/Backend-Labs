b953bcf1408ccda56505484f487aa50ed3be0d5a
mavagoncalves <mavagoncalves@gmail.com>
2026-03-11 18:19:57 +0100

    Added web setup

--- server.js
+++ server.js
@@ -4,17 +4,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const webRoutes = require('./routes/webRoutes');
 const helmet = require('helmet');
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+const webSetup = [
+    express.urlencoded({ extended: true }),
+    methodOverride('_method'),
+    session({
+        secret: 'my_super_secret_session',
+        resave: false,
+        saveUninitialized: false
+    }),
+    flash(),
+    (req, res, next) => {
+        res.locals.successMsg = req.flash('success');
+        res.locals.errorMsg = req.flash('error');
+        next();
+    }
+];
+
+app.use(webSetup);
+
+// security 
 app.use(helmet());
 app.disable('x-powered-by');
-
 app.use(express.json());
 
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
+
+// web routes
+app.use('/', webRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
   family: 4 //forces Node to use standard IPv4
